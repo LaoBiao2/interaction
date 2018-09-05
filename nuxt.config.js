14867a65cf7a39d6dafd12926ac9ccec1453df38
kwei <864816160@qq.com>
2018-09-05 13:59:10 +0800

    swiper

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
@@ -36,5 +36,11 @@ module.exports = {
       }
     }
   },
+  plugins: [
+    { src: '~/plugins/swiper.js', ssr: false }
+  ],
+  css: [
+    'swiper/dist/css/swiper.css'
+  ],
 }
 
